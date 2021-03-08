b15f52ce00bf3d28a5d917f862d297a146613ae2
CRAZYMIXAKER <zombisuki@gmail.com>
2021-03-08 17:04:56 +0300

    Another\DeadLine

---
+++ crud.php
@@ -0,0 +1,85 @@
+<?php
+
+class CRUD
+{
+	public $salt = "sheu2o5n21p59m0";
+	public $login;
+	public $xpath;
+	public $xml;
+	public $dom;
+
+	public function __construct()
+	{
+		$this->dom = $this->workWithXPATH();
+		$this->xpath = new DOMXPath($this->dom);
+		$this->xml = simplexml_load_file("db.xml");
+	}
+
+	public function createUser()
+	{
+		$add = $this->xml->addchild('user');
+		$add->addAttribute('name', $_POST['name']);
+		$add->addAttribute('email', $_POST['email']);
+		$add->addAttribute('login', $_POST['login']);
+		$add->addAttribute('password', md5(md5($_POST['password']) . $this->salt));
+		$add->addAttribute('access', 0);
+		$this->xml->saveXML('db.xml');
+
+		$_SESSION['User'] = [
+			"name" => $_POST['name'],
+			"email" => $_POST['email'],
+			"login" => $_POST['login'],
+			"access" => 0
+		];
+
+		setcookie("Login", $_SESSION['User']['login'], time() + 3600);
+		return $_SESSION['User'];
+	}
+
+	public function readUser($login)
+	{
+		$this->login = $login;
+		$result = $this->xpath->query("/users/user[@login='$this->login']");
+		foreach ($result as $node) {
+			$_SESSION['User'] = [
+				"name" => $node->getAttribute('name'),
+				"email" => $node->getAttribute('email'),
+				"login" => $node->getAttribute('login'),
+				"access" => $node->getAttribute('access')
+			];
+			setcookie("Login", $_SESSION['User']['login'], time() + 3600);
+			return $_SESSION['User'];
+		}
+	}
+
+	public function updateUser($name, $email, $login, $loginMain)
+	{
+		$this->login = $login;
+		$name = $name;
+		$email = $email;
+		$loginMain = $loginMain;
+
+		foreach ($this->xpath->query("/users/user[@login='$loginMain']") as $item) {
+			$item->setAttribute('name', "$name");
+			$item->setAttribute('email', "$email");
+			$item->setAttribute('login', "$this->login");
+		}
+
+		$this->dom->save('db.xml');
+	}
+
+	public function deleteUser($login)
+	{
+		$this->login = $login;
+		$result = $this->xml->xpath("//user[@login='$this->login']");
+		unset($result[0][0]);
+		$this->xml->asXML('db.xml');
+	}
+
+	public function workWithXPATH()
+	{
+		$dom = new DomDocument("1.0");
+		$dom->load("db.xml");
+		return $dom;
+	}
+}
--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php session_start();
-$salt = "sheu2o5n21p59m0";
+include_once("crud.php");
+
+$workWithXML = new CRUD();
+$xpath = $workWithXML->xpath;
+$salt = $workWithXML->salt;
 
 $responce = [
 	'res' => false,
@@ -10,31 +14,16 @@ $responce = [
 ];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	if (isset($_POST['login_sign_in']) || isset($_POST['name'])) {
+	if (isset($_POST['login_sign_in']) || isset($_POST['name']) || isset($_POST['editName'])) {
 		if (isset($_POST['login_sign_in']) && isset($_POST['password_sign_in'])) {
 			if (!empty(trim($_POST['login_sign_in'])) && !empty(trim($_POST['password_sign_in']))) {
-
-				$xpath = workWithXPATH();
-				$xml = simplexml_load_file("db.xml");
-
 				$login = trim($_POST['login_sign_in']);
 				$password = trim($_POST['password_sign_in']);
-
-
 				$checkPassword = md5(md5($password) . $salt);
 				$checkUser = $xpath->query("/users/user[@login = '$login' and @password = '$checkPassword']");
-
 				if ($checkUser->length == 1) {
-					$result = $xpath->query("/users/user[@login='$login']");
-
-					foreach ($result as $node) {
-						$_SESSION['User'] = [
-							"name" => $node->getAttribute('name'),
-							"email" => $node->getAttribute('email'),
-							"login" => $node->getAttribute('login')
-						];
-					}
-
+					$crudRead = new CRUD();
+					$crudRead->readUser($login);
 					$responce['res'] = true;
 				} else {
 					$responce['error'] = 'Неправильный логин или пароль';
@@ -45,32 +34,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		} elseif (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['login']) && isset($_POST['password']) && isset($_POST['confirm_password'])) {
 			if (!empty(trim($_POST['name'])) && !empty(trim($_POST['email'])) && !empty(trim($_POST['login'])) && !empty(trim($_POST['password'])) && !empty(trim($_POST['confirm_password']))) {
 				if ($_POST['password'] == $_POST['confirm_password']) {
-
-					$xpath = workWithXPATH();
-					$xml = simplexml_load_file("db.xml");
-
 					$login = trim($_POST['login']);
-
 					$checkLogin = $xpath->query("/users/user[@login = '$login']");
-
 					if ($checkLogin->length == 0) {
 						$email = $_POST['email'];
 						$checkEmail = $xpath->query("/users/user[@email = '$email']");
-
 						if ($checkEmail->length == 0) {
-							$add = $xml->addchild('user');
-							$add->addAttribute('name', $_POST['name']);
-							$add->addAttribute('email', $_POST['email']);
-							$add->addAttribute('login', $_POST['login']);
-							$add->addAttribute('password', md5(md5($_POST['password']) . $salt));
-							$xml->saveXML('db.xml');
-
-							$_SESSION['User'] = [
-								"name" => $_POST['name'],
-								"email" => $_POST['email'],
-								"login" => $_POST['login']
-							];
-
+							$crudAdd = new CRUD();
+							$crudAdd->createUser();
 							$responce['res'] = true;
 						} else {
 							$responce['errorEmail'] = 'Пользователь, с такой почтой уже существует, выберите пожалуйста другую почту';
@@ -84,16 +55,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			} else {
 				$responce['error'] = 'Поля не должны быть пустыми или заполнены пробелами';
 			}
+		} elseif (isset($_POST['editName']) && isset($_POST['editEmail']) && isset($_POST['editLogin'])) {
+			if (!empty(trim($_POST['editName'])) && !empty(trim($_POST['editEmail'])) && !empty(trim($_POST['editLogin']))) {
+				$crudUpdate = new CRUD();
+				$crudUpdate->updateUser($_POST['editName'], $_POST['editEmail'], $_POST['editLogin'], $_POST['editLoginMain']);
+				$responce['res'] = true;
+			} else {
+				$responce['error'] = 'Поля не должны быть пустыми или заполнены пробелами';
+			}
 		}
 	}
-}
-
-function workWithXPATH()
-{
-	$dom = new DomDocument("1.0");
-	$dom->load("db.xml");
-	$xpath = new DomXPath($dom);
-	return $xpath;
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+	if (isset($_GET['deleteLogin'])) {
+		if ($_SESSION['User']['login'] === $_GET['deleteLogin']) {
+			$_SESSION['Error'] = 'Вы не можете себя удалить';
+		} else {
+			$crudDelete = new CRUD();
+			$crudDelete->deleteUser($_GET['deleteLogin']);
+			$responce['res'] = true;
+		}
+		header("Location: index.php");
+	}
 }
 
 echo json_encode($responce);
